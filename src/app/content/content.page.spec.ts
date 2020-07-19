c717b0a0cefaf403f3c0eb3a5384e60b04d7c5a5
Thomas Burk <t.burk@shellraiser.com>
2020-07-19 09:25:30 +0200

    Set up basic unit tests

--- src/app/content/content.page.spec.ts
+++ src/app/content/content.page.spec.ts
@@ -1,16 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { ContentPage } from './content.page';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { ContentPage } from "./content.page";
+import { provideMockStore } from "@ngrx/store/testing";
 
-describe('ContentPage', () => {
+describe("ContentPage", () => {
   let component: ContentPage;
   let fixture: ComponentFixture<ContentPage>;
 
   beforeEach(async(() => {
+    const initialState = { sqrt: { input: 0, output: 0 } };
+
     TestBed.configureTestingModule({
-      declarations: [ ContentPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ContentPage],
+      providers: [provideMockStore({ initialState })],
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ContentPage);
@@ -18,7 +22,7 @@ describe('ContentPage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
